52044af277b58d5e483671cef3df0142d23c2fb7
glindy <glindy@udel.edu>
2021-09-30 10:28:16 -0400

    temp disabling index.tsx components until they have content

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+/*
 function square(props) {
   return (
 
@@ -38,7 +38,7 @@ class Game extends React.Component {
     );
   }
 }
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
