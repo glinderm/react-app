20b61b6a78b08537adb53750be20285051576c0d
glindy <glindy@udel.edu>
2021-09-30 15:35:57 -0400

    building board/game

--- src/index.tsx
+++ src/index.tsx
@@ -4,42 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-/*
-function square(props) {
-  return (
-
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
   );
-}
 
-class Board extends React.Component {
-  render() {
-    return (
-
-    );
-  }
-}
-
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    }
-  }
-  render() {
-    return (
-
-    );
-  }
-}
-*/
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
