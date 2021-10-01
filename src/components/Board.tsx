9fa613a7b6bf09132c76451963c647d48bf66067
glindy <glindy@udel.edu>
2021-09-30 20:33:53 -0400

    converted tutorial tic-tac-toe to use hooks

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,25 +4,37 @@ import ReactDOM from 'react-dom';
 import { Square } from './Square';
 
 export function Board() {
-    
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function renderSquare(i) {
+        return <Square
+            value={squares[i]}
+            onClick={() => {
+                const nextSquares = squares.slice();
+                nextSquares[i] = 'X';
+                setSquares(nextSquares);
+            }}
+        />;
+    }
+
     return (
         <div className="container">
             <div className="game">
                 <div className="game-board">
                 <div className="board-row">
-                    <Square value={0} onClick={null}/>
-                    <Square value={1} onClick={null}/>
-                    <Square value={2} onClick={null}/>
+                    {renderSquare(0)}
+                    {renderSquare(0)}
+                    {renderSquare(0)}
                 </div>
                 <div className="board-row">
-                    <Square value={3} onClick={null}/>
-                    <Square value={4} onClick={null}/>
-                    <Square value={5} onClick={null}/>
+                    {renderSquare(0)}
+                    {renderSquare(0)}
+                    {renderSquare(0)}
                 </div>
                 <div className="board-row">
-                    <Square value={6} onClick={null}/>
-                    <Square value={7} onClick={null}/>
-                    <Square value={8} onClick={null}/>
+                    {renderSquare(0)}
+                    {renderSquare(0)}
+                    {renderSquare(0)}
                 </div>
                 </div>
             </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+/* help converting tic tac toe chassis to use hooks https://medium.com/@shifrb/how-to-build-tic-tac-toe-with-react-hooks-ca37f6040022 */
 
 export function Square({ value, onClick }) {
   return (
@@ -13,30 +14,84 @@ export function Square({ value, onClick }) {
   );
 }
 
-export function Board() {
-    
+function Restart({ onClick }) {
+
   return (
-      <div className="container">
-          <div className="game">
-              <div className="game-board">
-              <div className="board-row">
-                  <Square value={0} onClick={null}/>
-                  <Square value={1} onClick={null}/>
-                  <Square value={2} onClick={null}/>
-              </div>
-              <div className="board-row">
-                  <Square value={3} onClick={null}/>
-                  <Square value={4} onClick={null}/>
-                  <Square value={5} onClick={null}/>
-              </div>
-              <div className="board-row">
-                  <Square value={6} onClick={null}/>
-                  <Square value={7} onClick={null}/>
-                  <Square value={8} onClick={null}/>
-              </div>
-              </div>
+    <button className="restart" onClick={onClick}>
+      Play again
+    </button>
+  );
+}
+
+export function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [ isPlayerTurn, setIsPlayerTurn ] = useState(true);
+  const nextPlayer = isPlayerTurn ? "X" : "O";
+  const winner = calculateWinner(squares);
+
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          if (squares[i] != null || winner != null) {
+            return;
+          }
+          const nextSquares = squares.slice();
+          nextSquares[i] = nextPlayer;
+          setSquares(nextSquares);
+
+          setIsPlayerTurn(!isPlayerTurn); // toggle turns
+        }}
+      />
+    );
+  }
+
+  function renderRestartButton() {
+    return (
+      <Restart
+        onClick={() => {
+          setSquares(Array(9).fill(null));
+          setIsPlayerTurn(true);
+        }}
+      />
+    );
+  }
+
+  function getStatus() {
+    if (winner) {
+      return "Winner: " + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw!";
+    } else {
+      return "Next player: " + nextPlayer;
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="game">
+        <div className="game-board">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
           </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+        <div className="game-info">{getStatus()}</div>
+        <div className="restart-button">{renderRestartButton()}</div>
       </div>
+    </div>
   );
 }
 
@@ -50,4 +105,35 @@ ReactDOM.render(
     document.getElementById('root')
   );
 */
+
+function calculateWinner(squares) {
+  const possibleLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  // go over all possibly winning lines and check if they consist of only X's/only O's
+  for (let i = 0; i < possibleLines.length; i++) {
+    const [a, b, c] = possibleLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function isBoardFull(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] == null) {
+      return false;
+    }
+  }
+  return true;
+}
+
 reportWebVitals();
